752a4b31bc1db1f348e47e150df039a9b836b1e7
sapta <www.saptarsi@gmail.com>
2025-02-18 21:06:09 +0100

    rpc-url-revert

--- app/src/main/java/cryptooperations/MyWeb3Client.java
+++ app/src/main/java/cryptooperations/MyWeb3Client.java
@@ -44,8 +44,8 @@ import java.util.regex.Pattern;
 
 public class MyWeb3Client {
     private Web3j web3j;
-    private static final String WEB3_RPC_PROVIDER = "<<MAINNET_RPC>>";
-    private static final String CONTRACT_ADDRESS = "<<MAINNET_CONTRACT_ADDRESS>>";
+    private static final String WEB3_RPC_PROVIDER = "https://rpc.ankr.com/electroneum_testnet/12ec0fbb73dde2bba66af4a7f50f036e17a19c90be52ff33a78976a15ad50229";
+    private static final String CONTRACT_ADDRESS = "0xBE5AA6040eED70d13DC196bBE3159379cF220D8c";
     private KeyGenerator keyGenerator;
     private Etnvegas etnvegas;
     public static final String ERROR_OCCURED = "error";
